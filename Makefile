da9fedbdb2a71474129c135453e026ae25ce53b4
Yuoa <contact@yuoa.pm>
2018-10-06 23:08:13 +0900

    MatrixMultiplication 1_1 Implemented

--- Makefile
+++ Makefile
@@ -1,15 +1,14 @@
 watch: InvertedIndex.jar Join.jar
 
 clean:
-	@rm InvertedIndex.jar
-	@rm InvertedIndex/InvertedIndex*.class
-	@rm ii.output
-	@rm Join.jar
-	@rm Join/Join*.class
-	@rm j.output
+	@rm InvertedIndex.jar InvertedIndex/InvertedIndex*.class ii.output
+	@rm Join.jar Join/Join*.class j.output
+	@rm MatrixMultiplication1_1.jar MatrixMultiplication/Multiplication1_1*.class m11.output
+	@rm MatrixMultiplication1_2.jar MatrixMultiplication/Multiplication1_2*.class m12.output
+	@rm MatrixMultiplication2.jar MatrixMultiplication/Multiplication2*.class m2.output
 
 test:
-	@echo "Enter \"testii\" or \"testj\"."
+	@echo "Enter \"testii\", \"testj\", \"testm11\", \"testm12\", or \"testm2\"."
 
 testii: InvertedIndex.jar
 	@echo -e "\033[1;37mInitializing HDFS structure...\033[0;37m"
@@ -18,7 +17,7 @@ testii: InvertedIndex.jar
 	@hdfs dfs -put -f InvertedIndex/input/* /user/input
 	@echo -e "\033[1;37m\nRunning InvertedIndex Program...\033[0;37m"
 	@hadoop jar InvertedIndex.jar InvertedIndex /user/input /user/output
-	@echo -e "\033[1;37m\nOutput will saved to <ii.output>\033[0;37m"
+	@echo -e "\033[1;37m\nOutput will be saved to <ii.output>\033[0;37m"
 	@hdfs dfs -get -f /user/output/part-r-00000 ii.output
 	@echo -e "\033[1;37m\nDifferences\033[0;37m"
 	@-diff InvertedIndex/output/part-r-00000 ii.output
@@ -31,10 +30,40 @@ testj: Join.jar
 	@hdfs dfs -put -f Join/data/* /user/input
 	@echo -e "\033[1;37m\nRunning Join Program...\033[0;37m"
 	@hadoop jar Join.jar Join /user/input/records /user/output/join order,line_item 1 1
-	@echo -e "\033[1;37m\nOutput will saved to <j.output>\033[0;37m"
+	@echo -e "\033[1;37m\nOutput will be saved to <j.output>\033[0;37m"
 	@hdfs dfs -get -f /user/output/join/part-r-00000 j.output
 	@echo -e "\033[1;37m\nDifferences\033[0;37m"
 	@-diff Join/output/part-r-00000 j.output
+	@echo -e "\033[0m"
+	
+testm11: MatrixMultiplication1_1.jar
+	@echo -e "\033[1;37mInitializing HDFS structure...\033[0;37m"
+	@hdfs dfs -rm -r -f /user/input /user/output
+	@hdfs dfs -mkdir -p /user /user/input
+	@hdfs dfs -put -f MatrixMultiplication/data/* /user/input
+	@echo -e "\033[1;37m\nRunning Multiplication1_1 Program...\033[0;37m"
+	@hadoop jar MatrixMultiplication1_1.jar Multiplication1_1 /user/input/matrix1_1 /user/output/multiple1_1 3 5 2
+	@echo -e "\033[1;37m\nOutput will be saved to <m11.output>\033[0;37m"
+	@hdfs dfs -get -f /user/output/multiple1_1/part-r-00000 m11.output
+	@echo -e "\033[1;37m\nDifferences\033[0;37m"
+	@-diff MatrixMultiplication/output/multiple1_1/part-r-00000 m11.output
+	@echo -e "\033[0m"
+
+testm12: MatrixMultiplication1_2.jar
+	@echo -e "\033[1;37mInitializing HDFS structure...\033[0;37m"
+	@hdfs dfs -rm -r -f /user/input /user/output
+	@hdfs dfs -mkdir -p /user /user/input
+	@hdfs dfs -put -f MatrixMultiplication/data/* /user/input
+	@echo -e "\033[1;37m\nRunning Multiplication1_2 Program (Phase 2)...\033[0;37m"
+	@hadoop jar MatrixMultiplication1_2.jar Mutliplication1_2 /user/input/matrix1_1 /user/input/matrix1_2 /user/output/multiple1_2 3 5 2 2
+	@echo -e "\033[1;37m\nOutput from phase 1 will be saved to <m11.output>\033[0;37m"
+	@hdfs dfs -get -f /user/output/multiple1_2/part-r-00000 m11.output
+	@echo -e "\033[1;37m\nOutput from phase 2 will be saved to <m12.output>\033[0;37m"
+	@hdfs dfs -get -f /user/output/multiple1_2/final/part-r-00000 m12.output
+	@echo -e "\033[1;37m\nDifferences from phase 1\033[0;37m"
+	@-diff MatrixMultiplication/output/multiple1_2/part-r-00000 m11.output
+	@echo -e "\033[1;37m\nDifferences from phase 2\033[0;37m"
+	@-diff MatrixMultiplication/output/multiple1_2/final/part-r-00000 m12.output
 	@echo -e "\033[0m"
 	
 InvertedIndex.jar: InvertedIndex/InvertedIndex.java
@@ -43,11 +72,32 @@ InvertedIndex.jar: InvertedIndex/InvertedIndex.java
 	@cd InvertedIndex; jar cf InvertedIndex.jar InvertedIndex*.class
 	@mv InvertedIndex/InvertedIndex.jar .
 	@echo -e "\033[0m"
-	
+		
 Join.jar: Join/Join.java
 	@echo -e "\033[1;37mBuild: Join.jar\033[0;37m"
 	@cd Join; hadoop com.sun.tools.javac.Main Join.java
 	@cd Join; jar cf Join.jar Join*.class
 	@mv Join/Join.jar .
+	@echo -e "\033[0m"
+
+MatrixMultiplication1_1.jar: MatrixMultiplication/Multiplication1_1.java
+	@echo -e "\033[1;37mBuild: MatrixMultiplication1_1.jar\033[0;37m"
+	@cd MatrixMultiplication; hadoop com.sun.tools.javac.Main Multiplication1_1.java
+	@cd MatrixMultiplication; jar cf MatrixMultiplication1_1.jar Multiplication1_1*.class
+	@mv MatrixMultiplication/MatrixMultiplication1_1.jar .
+	@echo -e "\033[0m"
+
+MatrixMultiplication1_2.jar: MatrixMultiplication/Multiplication1_1.java MatrixMultiplication/Multiplication1_2.java
+	@echo -e "\033[1;37mBuild: MatrixMultiplication1_2.jar\033[0;37m"
+	@cd MatrixMultiplication; hadoop com.sun.tools.javac.Main Multiplication1_2.java Multiplication1_1.java
+	@cd MatrixMultiplication; jar cf MatrixMultiplication1_2.jar Multiplication1_2*.class Multiplication1_1*.class
+	@mv MatrixMultiplication/MatrixMultiplication1_2.jar .
+	@echo -e "\033[0m"
+
+MatrixMultiplication2.jar: MatrixMultiplication/Multiplication2.java
+	@echo -e "\033[1;37mBuild: MatrixMultiplication2.jar\033[0;37m"
+	@cd MatrixMultiplication; hadoop com.sun.tools.javac.Main Multiplication2.java
+	@cd MatrixMultiplication; jar cf MatrixMultiplication2.jar Multiplication2*.class
+	@mv MatrixMultiplication/MatrixMultiplication2.jar .
 	@echo -e "\033[0m"
 
--- MatrixMultiplication/Multiplication1_1.java
+++ MatrixMultiplication/Multiplication1_1.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -8,62 +9,119 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 
-
 public class Multiplication1_1 {
 
-	// Complete the Matrix1_1_Mapper class. 
+	// Complete the Matrix1_1_Mapper class.
 	// Definitely, Generic type (Text, Text, Text, Text) must not be modified
-	
+
 	// Optional, you can add and use new methods in this class
-	// Optional, you can use both 'setup' and 'cleanup' function, or either of them, or none of them.
-	
+	// Optional, you can use both 'setup' and 'cleanup' function, or either of them,
+	// or none of them.
+
 	public static class Matrix1_1_Mapper extends Mapper<Text, Text, Text, Text> {
 		// Optional, Using, Adding, Modifying and Deleting variable is up to you
 		int n_first_rows = 0;
-		int n_first_cols = 0;
 		int n_second_cols = 0;
-		private String newEntry = null;
-		
+
 		// Optional, Utilizing 'setup' function or not is up to you
 		protected void setup(Context context) throws IOException, InterruptedException {
 			n_first_rows = context.getConfiguration().getInt("n_first_rows", 0);
-			n_first_cols = context.getConfiguration().getInt("n_first_cols", 0);
 			n_second_cols = context.getConfiguration().getInt("n_second_cols", 0);
 		}
-				
-		// Definitely, parameter type and name (Text matrix, Text entry, Context context) must not be modified
+
+		// Definitely, parameter type and name (Text matrix, Text entry, Context
+		// context) must not be modified
 		public void map(Text matrix, Text entry, Context context) throws IOException, InterruptedException {
 			// Implement map function.
+
+			String[] record = entry.toString().split(",");
+
+			if (matrix.toString().equals("a")) {
+
+				for (int k = 0; k < n_second_cols; k++) {
+
+					context.write(new Text(record[0] + "," + k), new Text(record[1] + "," + record[2]));
+
+				}
+
+			} else {
+
+				for (int k = 0; k < n_first_rows; k++) {
+
+					context.write(new Text(k + "," + record[1]), new Text(record[0] + "," + record[2]));
+
+				}
+
+			}
+
 		}
+
 	}
 
-	// Complete the Matrix1_1_Reducer class. 	
+	// Complete the Matrix1_1_Reducer class.
 	// Definitely, Generic type (Text, Text, Text, Text) must not be modified
-	// Definitely, Output format and values must be the same as given sample output 
-	
-	// Optional, you can use both 'setup' and 'cleanup' function, or either of them, or none of them.
+	// Definitely, Output format and values must be the same as given sample output
+
+	// Optional, you can use both 'setup' and 'cleanup' function, or either of them,
+	// or none of them.
 	// Optional, you can add and use new methods in this class
 	public static class Matrix1_1_Reducer extends Reducer<Text, Text, Text, Text> {
 		// Optional, Using, Adding, Modifying and Deleting variable is up to you
-		int n_first_rows = 0;
 		int n_first_cols = 0;
-		int n_second_cols = 0;
-		
+
 		// Optional, Utilizing 'setup' function or not is up to you
 		protected void setup(Context context) throws IOException, InterruptedException {
-			n_first_rows = context.getConfiguration().getInt("n_first_rows", 0);
 			n_first_cols = context.getConfiguration().getInt("n_first_cols", 0);
-			n_second_cols = context.getConfiguration().getInt("n_second_cols", 0);
 		}
-		
-		// Definitely, parameters type (Text, Iterable<Text>, Context) must not be modified
+
+		// Definitely, parameters type (Text, Iterable<Text>, Context) must not be
+		// modified
 		// Optional, parameters name (key, values, context) can be modified
-		public void reduce(Text entry, Iterable<Text> entryComponents, Context context) throws IOException, InterruptedException {
+		public void reduce(Text entry, Iterable<Text> entryComponents, Context context)
+				throws IOException, InterruptedException {
 			// Implement reduce function.
+
+			int result = 0;
+			int[] partialResult = new int[n_first_cols];
+			boolean[] partialAssigned = new boolean[n_first_cols];
+			boolean[] partialCalculated = new boolean[n_first_cols];
+
+			for (Text rawValue : entryComponents) {
+
+				String[] value = rawValue.toString().split(",");
+				int partialIndex = Integer.parseInt(value[0]);
+
+				if (partialAssigned[partialIndex]) {
+
+					partialResult[partialIndex] *= Integer.parseInt(value[1]);
+					partialCalculated[partialIndex] = true;
+
+				} else {
+
+					partialResult[partialIndex] = Integer.parseInt(value[1]);
+					partialAssigned[partialIndex] = true;
+
+				}
+
+			}
+
+			for (int i = 0; i < n_first_cols; i++) {
+
+				if (partialCalculated[i]) {
+
+					result += partialResult[i];
+
+				}
+
+			}
+
+			context.write(entry, new Text(Integer.toString(result)));
+
 		}
+
 	}
-	
-	// Definitely, Main function must not be modified 
+
+	// Definitely, Main function must not be modified
 	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "Matrix Multiplication1_1");
@@ -81,7 +139,8 @@ public class Multiplication1_1 {
 		job.getConfiguration().setInt("n_first_rows", Integer.parseInt(args[2]));
 		job.getConfiguration().setInt("n_first_cols", Integer.parseInt(args[3]));
 		job.getConfiguration().setInt("n_second_cols", Integer.parseInt(args[4]));
-		
+
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
+
 }
